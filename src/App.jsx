ada0ee9a959412238a262399a6ebb29813f7190b
Lira <werda1818@gmail.com>
2026-06-14 20:16:59 +0300

    removed comments from the main App component

--- src/App.jsx
+++ src/App.jsx
@@ -2,41 +2,6 @@ import { Navigate, Routes, Route } from "react-router-dom";
 import styles from "./app.module.css";
 import { TaskPage, TasksListPage, NotFoundPage } from "./pages";
 
-
-// function AppContent() {
-
-//     return (
-//         <Routes>
-//             <Route 
-//                 path="/" 
-//                 element={
-//                     <>
-//                         <div className="container">
-//                             <section className={styles["todos-app"]}>
-//                                 <h1 className={styles.title}>Todos application</h1>
-
-
-//                             </section>
-//                         </div>
-//                     </>
-//                 } 
-//             />
-//             <Route 
-//                 path="/task/:id" 
-//                 element={
-//                     <TaskPage 
-//                         todos={todos} 
-//                         isLoading={isLoading} 
-//                         onEdit={editTodo} 
-//                         onDelete={deleteTodo} 
-//                     /> 
-//                 } 
-//             />
-//             <Route path="*" element={<NotFound />} />
-//         </Routes>
-//     );
-// }
-
  const App = () => {
     return (
         <div className={styles['todos-app']}>
